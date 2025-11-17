911f7bd21c1285ed18b6c6b0b8c3a0f41ce943c1
majamakowska <maja.makowska05@gmail.com>
2025-11-17 23:18:40 +0100

    RED: test shouldUpdateSalaryWithPromotion

--- employee-system-mpr/src/test/java/org/example/service/PromotionServiceTest.java
+++ employee-system-mpr/src/test/java/org/example/service/PromotionServiceTest.java
@@ -3,6 +3,9 @@ package org.example.service;
 import org.example.model.Employee;
 import org.example.model.Position;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PromotionServiceTest {
@@ -16,5 +19,15 @@ class PromotionServiceTest {
         promotionService.promote(employee, Position.MANAGER);
 
         assertEquals(Position.MANAGER, employee.getPosition());
+    }
+
+    @Test
+    void shouldUpdateSalaryWithPromotion() {
+        PromotionService promotionService = new PromotionService();
+        Employee employee = new Employee("Anna", "Nowak",
+                "anna.nowak@test.com", "Firma X", Position.PROGRAMISTA);
+
+        promotionService.promote(employee, Position.MANAGER);
+        assertThat(employee.getSalary(), is(equalTo(12000.00)));
     }
 }
